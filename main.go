abd3561462a8b0336a98d757255ad03660212150
Nik Efimov <nikkefimov@gmail.com>
2024-11-14 00:15:51 +0100

    create server

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
 
 	"github.com/nikkefimov/grpc/go_service"
+	"google.golang.org/grpc"
 )
 
 type go_serviceServer struct {
@@ -18,5 +21,19 @@ func (s go_serviceServer) Create(context.Context, *go_service.CreateRequest) (*g
 }
 
 func main() {
+	lis, err := net.Listen("tcp", ":8088")
+	if err != nil {
+		log.Fatalf("can't create listner: %s", err)
+	}
+
+	serverRegistrar := grpc.NewServer()
+	service := &go_serviceServer{}
+
+	go_service.RegisterGoServiceServer(serverRegistrar, service)
+
+	err = serverRegistrar.Serve(lis)
+	if err != nil {
+		log.Fatalf("serve error: %s", err)
+	}
 
 }
